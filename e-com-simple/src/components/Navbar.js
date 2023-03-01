5772052791bf5e358e99af75039f24a64b88386f
arumbaua366 <arumbaua366@gmail.com>
2023-02-28 23:50:10 -0800

    button setup in navbar component

--- e-com-simple/src/components/Navbar.js
+++ e-com-simple/src/components/Navbar.js
@@ -1,7 +1,13 @@
 import { Button, Container, Navbar, Modal, Nav } from "react-bootstrap";
 
 function NavbarComponent() {
-  <Navbar expand="sm"></Navbar>;
+  <Navbar expand="sm">
+    <Navbar.Brand href="/">We-Comm</Navbar.Brand>
+    <Navbar.Toggle />
+    <Navbar.Collapse className="justify-content-end">
+      <Button>Cart 0 Items</Button>
+    </Navbar.Collapse>
+  </Navbar>;
 }
 
 export default NavbarComponent;

4ca4f1fd2f86d11b8e554e43278c0f08d8e54002
arumbaua366 <arumbaua366@gmail.com>
2023-03-03 00:32:12 -0800

    add router

--- e-com-simple/src/App.js
+++ e-com-simple/src/App.js
@@ -3,11 +3,18 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavbarComponent from "./components/Navbar";
 import { Container } from "react-bootstrap";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Container>
       <NavbarComponent></NavbarComponent>;
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<We-Comm />} />
+          <Route path="success" element={<We-Comm />} />
+        </Routes>
+      </BrowserRouter>
     </Container>
   );
 }

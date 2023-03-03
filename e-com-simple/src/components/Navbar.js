87d2adad87bc36c69036339ccb2f9fb6759f0849
arumbaua366 <arumbaua366@gmail.com>
2023-03-03 00:24:18 -0800

    fixed NavbarComponent output

--- e-com-simple/src/components/Navbar.js
+++ e-com-simple/src/components/Navbar.js
@@ -1,13 +1,15 @@
 import { Button, Container, Navbar, Modal, Nav } from "react-bootstrap";
 
 function NavbarComponent() {
-  <Navbar expand="sm">
-    <Navbar.Brand href="/">We-Comm</Navbar.Brand>
-    <Navbar.Toggle />
-    <Navbar.Collapse className="justify-content-end">
-      <Button>Cart 0 Items</Button>
-    </Navbar.Collapse>
-  </Navbar>;
+  return (
+    <Navbar expand="sm">
+      <Navbar.Brand href="/">We-Comm</Navbar.Brand>
+      <Navbar.Toggle />
+      <Navbar.Collapse className="justify-content-end">
+        <Button>Cart 0 Items</Button>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
 
 export default NavbarComponent;

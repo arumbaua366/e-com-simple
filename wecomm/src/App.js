6bb68fe23756d7957ad75af2b91c7530856646e1
arumbaua366 <arumbaua366@gmail.com>
2023-03-05 23:57:29 -0800

    add cancel, main page, and success pages for transactions

--- wecomm/src/App.js
+++ wecomm/src/App.js
@@ -2,9 +2,28 @@ import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavbarComponent from "./components/Navbar";
+import { Container } from "react-bootstrap";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Cancel from "./pages/Cancel";
+import Success from "./pages/Success";
+import Store from "./pages/Store";
+
+// localhost: 3001 -> Home
+// localhoust:3001/success -> Success
 
 function App() {
-  return <NavbarComponent></NavbarComponent>;
+  return (
+    <Container>
+      <NavbarComponent></NavbarComponent>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Store />} />
+          <Route path="success" element={<Success />} />
+          <Route path="cancel" element={<Cancel />} />
+        </Routes>
+      </BrowserRouter>
+    </Container>
+  );
 }
 
 export default App;
---
+++ wecomm/src/pages/Store.js
@@ -0,0 +1,5 @@
+function Store() {
+  return <h1>Welcome to We Comm!</h1>;
+}
+
+export default Store;

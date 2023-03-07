d6ebe642f2173fbfac0793ef580035771764e6a5
arumbaua366 <arumbaua366@gmail.com>
2023-03-06 23:45:56 -0800

    edit products page to map through product list

--- wecomm/src/pages/Store.js
+++ wecomm/src/pages/Store.js
@@ -1,25 +1,17 @@
 import { Row, Col } from "react-bootstrap";
+import { productsArray } from "../productsStore";
+// [{...}, {...}, {...}]
 
 function Store() {
   return (
     <>
       <h1>Welcome to We Comm!</h1>
       <Row xs={1} md={3} className="g-4">
-        <Col align="center">
-          <h1>Product</h1>
-        </Col>
-        <Col align="center">
-          <h1>Product</h1>
-        </Col>
-        <Col align="center">
-          <h1>Product</h1>
-        </Col>
-        <Col align="center">
-          <h1>Product</h1>
-        </Col>
-        <Col align="center">
-          <h1>Product</h1>
-        </Col>
+        {productsArray.map((product, idx) => (
+          <Col align="center" key={idx}>
+            <h1>{product.title}</h1>
+          </Col>
+        ))}
       </Row>
     </>
   );

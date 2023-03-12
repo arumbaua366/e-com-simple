5d1f31af76a2ef3ac229795419cfa39871e85501
arumbaua366 <arumbaua366@gmail.com>
2023-03-11 23:42:23 -0800

    update card button to add one item at a time to cart

--- wecomm/src/components/ProductCard.js
+++ wecomm/src/components/ProductCard.js
@@ -1,15 +1,21 @@
 import { Card, Button, Form, Row, Col } from "react-bootstrap";
+import { CartContext } from "../CartContext";
+import { useContext } from "react";
 
 function ProductCard(props) {
   // props.product is the product we are selling
   const product = props.product;
+  const cart = useContext(CartContext);
+  const getProductQuantity = cart.getProductQuantity(product);
 
   return (
     <Card>
       <Card.Body>
         <Card.Title>{product.title}</Card.Title>
         <Card.Text>${product.price}</Card.Text>
-        <Button variant="primary">Add To Cart</Button>
+        <Button variant="primary" onClick={() => cart.addOneToCart(product.id)}>
+          Add To Cart
+        </Button>
       </Card.Body>
     </Card>
   );
